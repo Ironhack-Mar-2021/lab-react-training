e88022f093dd660b7e681171fbb7410c73e6ead2
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-19 17:17:00 -0400

    ITeration 3/4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import IdCard from './IdCards'
 import Greetings from './Greetings'
+import Random from './Random';
+import BoxColor from './BoxColor';
 
 function App() {
   return (
@@ -25,6 +27,10 @@ function App() {
       />
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
+      <Random min={1} max={6}/>
+      <Random min={50} max={100}/>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
