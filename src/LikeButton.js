bf1361560ed411c29730c2501dd37a6d572d372c
dickycom <dickycom1@gmail.com>
2021-04-20 16:04:53 -0400

    likebutton

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -3,11 +3,22 @@ import React, { useState } from 'react';
 function LikeButton() {
   let [like1, setLike1] = useState(0);
   let [like2, setLike2] = useState(0);
+  const colorArr = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
 
   return (
     <div>
-      <button onClick={() => setLike1(++like1)}>{like1} Likes</button>
-      <button onClick={() => setLike2(++like2)}>{like2} Likes</button>
+      <button
+        style={{ backgroundColor: colorArr[like1 % colorArr.length] }}
+        onClick={() => setLike1(++like1)}
+      >
+        {like1} Likes
+      </button>
+      <button
+        style={{ backgroundColor: colorArr[like2 % colorArr.length] }}
+        onClick={() => setLike2(++like2)}
+      >
+        {like2} Likes
+      </button>
     </div>
   );
 }
